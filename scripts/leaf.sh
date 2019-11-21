62ef5043d975d77c677d5f74b5ecfda11021ad24
Sonia El Hedri <elhedrisonia@gmail.com>
2019-11-22 02:39:02 +0900

    Change script to run new leaf

--- scripts/leaf.sh
+++ scripts/leaf.sh
@@ -13,6 +13,8 @@ tag=r$run.$repeat
 # Inputs (vector files) and outputs
 inname=$lowfit_dir/$emin\_$emax
 infile=$inname/$lowfit_prefix\.$tag\.mcfit.root
+inname_zbs=$apfit_dir/$emin\_$emax
+infile_zbs=$inname/$apfit_prefix\.$tag\.root
 outname=$leaf_dir/$emin\_$emax
 mkdir -p $outname
 outfile=$outname/$leaf_prefix\.$tag.root
@@ -26,8 +28,8 @@ then
 fi
 
 # leaf events
-echo "$leaf $infile $outfile"
-time $leaf $infile $outfile
+echo "$leaf $outfile $infile $infile_zbs"
+time $leaf $outfile $infile $infile_zbs
 
 echo ' Check: '
 ls -haltr $outfile
